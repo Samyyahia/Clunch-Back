dfda49501a06424d1d2dd681629419716dde0124
Samy Yahia <s.yahia@subskill.com>
2018-12-18 22:10:25 +0100

    Init User Admin

---
+++ src/Clunch/AdminBundle/Admin/UserAdmin.php
@@ -0,0 +1,49 @@
+<?php
+namespace Clunch\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class UserAdmin extends AbstractAdmin
+{
+  protected $baseRouteName = 'admin_clunch_users';
+
+  protected $baseRoutePattern = 'users';
+
+  protected function configureFormFields(FormMapper $formMapper)
+  {
+    $formMapper
+              ->add('username', null, array(
+                'label'    => 'Nom d\'utilisateur'
+              ))
+              ->add('email')
+              ->add('enabled', CheckboxType::class, array(
+                'required' => false,
+                'label'    => 'Activer',
+              ));
+  }
+
+  protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+  {
+    $datagridMapper
+                  ->add('username', null, array('label' => 'Nom d\'utilisateur'))
+                  ->add('email', null, array('label' => 'Email'));
+  }
+
+  protected function configureListFields(ListMapper $listMapper)
+  {
+    unset($this->listModes['mosaic']);
+
+    $listMapper
+              ->addIdentifier('username', null, array('label' => 'Nom d\'utilisateur'))
+              ->add('email')
+              ->add('_action', 'actions', array(
+                'actions' => array(
+                  'edit' => array(),
+                )
+              ));
+  }
+}
