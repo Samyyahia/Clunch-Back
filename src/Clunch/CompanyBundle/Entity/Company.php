1a0b80019368100adb0c95aa92989cfb5d621537
Samy Yahia <s.yahia@subskill.com>
2018-12-23 15:29:27 +0100

    Added Company Entity

---
+++ src/Clunch/CompanyBundle/Entity/Company.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Clunch\CompanyBundle\Entity;
+
+/**
+ * Company
+ */
+class Company
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $token;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Company
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return Company
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+}
+
