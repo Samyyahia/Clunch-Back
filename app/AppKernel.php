630fdff2e9842718ce3cec1389ee4413b35700f4
Samy Yahia <s.yahia@subskill.com>
2019-01-06 23:51:08 +0100

    Initialisation IngredientBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -43,6 +43,7 @@ class AppKernel extends Kernel
             new Clunch\CommentBundle\CommentBundle(),
             new Clunch\RecipeBundle\RecipeBundle(),
             new Clunch\CategoryBundle\CategoryBundle(),
+            new Clunch\IngredientBundle\IngredientBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
