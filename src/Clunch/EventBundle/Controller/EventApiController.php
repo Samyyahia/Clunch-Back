ceb8ad2e61c7fe8f78d875a45956e190d2c5e4e5
Samy Yahia <s.yahia@subskill.com>
2019-04-05 09:24:49 +0200

    Up User Api Controller / Routing

---
+++ src/Clunch/EventBundle/Controller/EventApiController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samy Yahia
+ * Date: 2019-04-05
+ * Time: 09:19
+ */
+
+namespace Clunch\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EventApiController extends Controller
+{
+
+}
--- src/Clunch/UserBundle/Controller/UserApiController.php
+++ src/Clunch/UserBundle/Controller/UserApiController.php
@@ -10,11 +10,21 @@ namespace Clunch\UserBundle\Controller;
 
 use Clunch\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Class UserApiController
+ * @package Clunch\UserBundle\Controller
+ */
 class UserApiController extends Controller
 {
+    /**
+     * Function to get Category List From Category Entity
+     * Route: /api/categories
+     * Method: GET
+     *
+     * @return JsonResponse
+     */
     public function getUsersAction() {
         $serializer = $this->get('jms_serializer');
 
@@ -28,6 +38,10 @@ class UserApiController extends Controller
         return new JsonResponse($users);
     }
 
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
     public function getUserAction($id) {
         $serializer = $this->get('jms_serializer');
 
