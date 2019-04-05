e479e71f97a10ed5f01c80feb3aa76568cea65cd
Samy Yahia <s.yahia@subskill.com>
2019-04-05 09:41:23 +0200

    Added Event Api Controller

--- src/Clunch/EventBundle/Controller/EventApiController.php
+++ src/Clunch/EventBundle/Controller/EventApiController.php
@@ -8,9 +8,35 @@
 
 namespace Clunch\EventBundle\Controller;
 
+use Clunch\EventBundle\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Class EventApiController
+ * @package Clunch\EventBundle\Controller
+ */
 class EventApiController extends Controller
 {
+    /**
+     * Function to get Event Item by id
+     * Route: /api/events/{id}
+     * Method: GET
+     *
+     * @param $id
+     * @return JsonResponse
+     */
+    public function getEventAction($id)
+    {
+        $serializer = $this->get('jms_serializer');
 
+        $em = $this->getDoctrine()->getManager();
+
+        $eventRepository = $em->getRepository(Event::class);
+        $event = $eventRepository->find($id);
+
+        $event = $serializer->toArray($event);
+
+        return new JsonResponse($event);
+    }
 }
